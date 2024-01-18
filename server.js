2b874ab0310ae5779aebe4cba682762829b7de05
Tanmesh <tanmeshnm@gmail.com>
2024-01-18 14:29:40 -0800

    updating server.js

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-
+server.use(
+    // Add custom route here if needed
+    jsonServer.rewriter({
+     "/api/*": "/$1",
+    })
+   );
 server.use(router);
 // Listen to port
 server.listen(3000, () => {
